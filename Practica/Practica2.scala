8f0b422a0b8066144efc11b3f8aa777a049c6329
EdwinUrias <edwin.urias17@tectijuana.edu.mx>
2019-09-24 20:05:40 -0700

    test

--- Practica/Practica2.scala
+++ Practica/Practica2.scala
@@ -24,4 +24,4 @@ val nombres = collection.mutable.Map(("Jose", 20), ("Luis", 24), ("Ana", 23), ("
     //a . Imprime todas la llaves del mapa
     nombres.keys
     //b . Agrega el siguiente valor al mapa("Miguel", 23)
-    nombres += (("Miguel", 23)).
+    nombres += (("Miguel", 23))
