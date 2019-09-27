6eea6d97ce2e9dd093142ee90e90c6a8c46de832
EdwinUrias <edwin.urias17@tectijuana.edu.mx>
2019-09-27 13:36:55 -0700

    TEST

--- Practica/Practica6.scala
+++ Practica/Practica6.scala
@@ -1,7 +1,7 @@
 import org.apache.spark.sql.SparkSession
 val spark = SparkSession.builder().getOrCreate()
 val df = spark.read.option("header", "true").option("inferSchema","true")csv("CitiGroup2006_2008")
-df.printSchema()
+
 df.head(2)
 df.select(month(df("Date"))).show()
 df.select(year(df($"Date"))).show()
