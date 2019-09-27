b1f099629d11759db24f12fca6487f9ff1b6edb9
EdwinUrias <edwin.urias17@tectijuana.edu.mx>
2019-09-27 13:37:56 -0700

    test

--- Practica/Practica4.scala
+++ Practica/Practica4.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.sql.SparkSession
 val spark = SparkSession.builder().getOrCreate()
 val df = spark.read.option("header", "true").option("inferSchema","true")csv("CitiGroup2006_2008")
+
 df.show()
 df.count
 df.columns
