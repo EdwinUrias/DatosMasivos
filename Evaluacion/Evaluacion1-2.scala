d44dd990f58c6bf61591570e5ad33e7e8485c05c
EdwinUrias <edwin.urias17@tectijuana.edu.mx>
2019-09-30 17:39:51 -0700

    Resuelto

--- Evaluacion/Evaluacion1-2.scala
+++ Evaluacion/Evaluacion1-2.scala
@@ -1,4 +1,6 @@
 import org.apache.spark.sql.SparkSession
+import spark.implicits._
+
 //1
 val spark = SparkSession.builder().getOrCreate()
 //2
@@ -11,22 +13,31 @@ df.printSchema()
 df.head(5)
 df.show(5)
 //6 OPERACION PARA HACER UNA NUEVA COLUMNA
-
+df.describe()
 //7
-val  = df.HVRatio("Year", year(df("Date")))
+val df2 = df.withColumn("HV Ratio", df("High")/df("Volume"))
+df2.select("HV Ratio").show()
 //8
 //---------------------------------------------------------
 //9
-//RESPUESTA EN TEXTO
+print("El csv es una muestra del precio al que estaban las acciones en la bolsa de valores de la compania Netllix, la columna Close representa el precio con el que cerro el valor por accion de Netflix de ese dia.")
 //10
-df.select(max("Volume") && min("Volume")).show()
+df.select(max("Volume")).show()
+df.select(min("Volume")).show()
 //11
 //a)
 df.filter($"Close" < 600).count()
 //b)
-
+print((df.filter($"High">500).count()*100)/1250,"%")
 //c)
-
+df.select(corr("High", "Volume")).show()
 //d)
-
+val df2=df.withColumn("Year",year(df("Date")))
+val dfmax=df2.groupBy("Year").max()
+dfmax.select($"Year",$"max(High)").show()
+dfmax.select($"Year",$"max(High)").show(1)
 //e)
+val dfmonth=df.withColumn("Month",month(df("Date")))
+val dfmean=dfmonth.select($"Month",$"Close").groupBy("Month").mean()
+dfmean.orderBy($"Month".desc).show()
+dfmean.orderBy($"Month").show()
