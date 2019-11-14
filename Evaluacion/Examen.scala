a5407835a5b974ce286532848c6b929755d2e927
EdwinUrias <edwin.urias17@tectijuana.edu.mx>
2019-11-13 17:48:13 -0800

    test

--- Evaluacion/Examen.scala
+++ Evaluacion/Examen.scala
@@ -1,48 +1,36 @@
-
-
-//TODO ***************************************************************************************************************************************
-//******Librerias
-//import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.feature.StringIndexer
-import org.apache.spark.sql.types._
 import org.apache.spark.ml.Pipeline
-
-//Se crea la estructura de los datos por columnas
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.sql.types._
+import org.apache.spark.ml.feature.StringIndexer
+//Columnas de datos
 val structtype = StructType(StructField("c0", DoubleType, true) ::StructField("c1", DoubleType, true) ::StructField("c2", DoubleType, true) ::StructField("c3",DoubleType, true) ::StructField("c4", StringType, true) :: Nil)
-//Ahora cargamos el CSV como DataFrame con la estructura de columnas creada previamente
-val dfstruct = spark.read.option("header", "false").schema(structtype)csv("/media/erebus/ESD-USB/Evaluacion/Iris.csv")
-//Creamos la columna label
+//Carga del dataset
+val dfstruct = spark.read.option("header", "false").schema(structtype)csv("/home/erebus/DatosMasivos/Evaluacion/Iris.csv")
+//Columna etiqueta label
 val label = new StringIndexer().setInputCol("c4").setOutputCol("label")
-//Se crea un arreglo de los datos de las columnas c0-c3 en la columna features
+//Arreglo de datos de columnas c0 c1 c2 c3 en la columna features
 val assembler = new VectorAssembler().setInputCols(Array("c0", "c1", "c2", "c3")).setOutputCol("features")
-
-//Se separa los datos en dos grupos, uno para entrenar y el otro para prueba desde nuestro df
+//Separa los datos en dos grupos (split)para entrenar y ralizar prueba desde df
 val splits = dfstruct.randomSplit(Array(0.7, 0.3), seed = 1234L)
-val train = splits(0) //entrenar
-val test = splits(1) //prueba
-
-//Especificamos las capas de nuestra red neuronal
-//4 de entrada, dos capas internas, una de 5, otra de 4 neuronas y 3 de salida
-val layers = Array[Int](4, 7, 6, 3)
-
-//Creamos el entrenador y especificamos los parametros
-//.setLayers es para cargar las capas de nuestra red neuronal
-//.setMaxIter es para indicar el numero maximo de iteraciones 
+//Val de entrenamiento
+val train = splits(0) 
+//Val de pruebas
+val test = splits(1)
+//Especificamos las capas de nuestra red neuronal 4 neuronas de entrada, dos capas internasy 3 de salida
+val layers = Array[Int](4, 5, 3, 3)
+//Se crea el tester y se especifican los parametros. La propiedad "setLayers" carga las capas de la red neuronal y la pripiedad "setMaxIter" indica el numero maximo de iteraciones 
 val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setLabelCol("label").setFeaturesCol("features").setPredictionCol("prediction").setBlockSize(128).setSeed(1234L).setMaxIter(100)
-
-//Se genera un pipeline con los datos que necesitamos de la label y las features 
+//Genera un pipeline con los datos para el label y los features 
 val pipe = new Pipeline().setStages(Array(label,assembler,trainer))
-//Ahora se entrena el modelo con los datos anteriores
+//Con los datos entrena el modelo
 val model = pipe.fit(train)
-
-//Calculamos la exactitud en el conjunto test
+//Se calcula la presicion del test
 val res = model.transform(test)
-//Mostramos el resultado
+//Muestra el resultado
 res.show()
 val predictionAndLabels = res.select("prediction", "label")
 val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
-//Imprimimos los resultados de exactitud utilizando un evaluador multiclase
+//Imprime resultados de presicion utilizando un evaluador multiclase
 println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
