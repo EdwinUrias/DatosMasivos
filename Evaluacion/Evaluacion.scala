fa6e9b9d18954c5526f12b117d65ac4cd1e6023f
EdwinUrias <edwin.urias17@tectijuana.edu.mx>
2019-11-30 17:38:48 -0800

    setup

---
+++ Evaluacion/Evaluacion.scala
@@ -0,0 +1,24 @@
+//Las fuente de datos se encuentra en el repositorio: https://github.com/jcromerohdz/BigData/blob/master/Spark_clustering/Wholesalecustomersdata.csv
+//1. Importar una simple sesión Spark.
+
+//2. Utilice las lineas de código para minimizar errores
+
+//3. Cree una instancia de la sesión Spark
+
+//4. Importar la librería de Kmeans para el algoritmo de agrupamiento.
+
+//5. Carga el dataset de Wholesale Customers Data
+
+//6. Seleccione las siguientes columnas: Fres, Milk, Grocery, Frozen, Detergents_Paper,Delicassen y llamar a este conjunto feature_data
+
+//7. Importar Vector Assembler y Vector
+
+//8. Crea un nuevo objeto Vector Assembler para las columnas de caracteristicas como un conjunto de entrada, recordando que no hay etiquetas
+
+//9. Utilice el objeto assembler para transformar feature_data
+
+//10. Crear un modelo Kmeans con K=3
+
+//11. Evalúe los grupos utilizando
+
+//12. ¿Cuáles son los nombres de las columnas?
